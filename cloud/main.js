1f69683d7585bd0735103350cdc383fbc7e79843
flyingtimes <5200374@qq.com>
2015-09-02 01:33:41 +0800

    Create main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,5 +2,5 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Hello world from main.js!");
 });
