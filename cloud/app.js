2b396bc68dc9c8d7de8ae9ba77dfaba016d85299
flyingtimes <5200374@qq.com>
2015-09-02 16:38:52 +0800

    Create app.js

--- cloud/app.js
+++ cloud/app.js
@@ -17,6 +17,11 @@ app.use(AV.Cloud);
 // 使用 AV.Cloud.CookieSession 中间件启用 CookieSession，注意传入一个 secret 用于 cookie 加密（必须）。它会自动将AV.User的登录信息记录到 cookie 里，用户每次访问会自动检查用户是否已经登录，如果已经登录，可以通过 req.AV.user 获取当前登录用户。
 app.use(AV.Cloud.CookieSession({ secret: 'my secret', maxAge: 3600000, fetchUser: true }));
 
+// App 全局配置
+app.set('views','cloud/views');   // 设置模板目录
+app.set('view engine', 'ejs');    // 设置 template 引擎
+app.use(express.bodyParser());    // 读取请求 body 的中间件
+
 app.get('/login', function(req, res) {
   // 渲染登录页面
   res.render('login.ejs');
